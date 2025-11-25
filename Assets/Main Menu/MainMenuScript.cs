37b3bd8aacf9d346ca0bd6d22d79461fe8253b65
IAAreval0 <143051271+IAAreval0@users.noreply.github.com>
2025-11-24 22:22:58 -0800

    added exit button + functionality to the buttons

---
+++ Assets/Main Menu/MainMenuScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame() //starts game when press start button
+    {
+        SceneManager.LoadScene("Scene_1"); 
+        //REMINDERS:
+        //      - change string name from "Scene_1" to whatever the real scene will 
+        //          officially be called
+        //      - in build settings make sure to add the scenes that it will navigate to 
+        //             - how to get to build settings: file -> build settings
+    }
+
+    public void QuitGame() //quits game when press exit button
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
