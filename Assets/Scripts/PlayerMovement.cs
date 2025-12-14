d76b1115683d445cbbd0b21e3eec2813994a3337
IAAreval0 <143051271+IAAreval0@users.noreply.github.com>
2025-12-14 11:12:46 -0800

    removed sound effect from player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,10 +19,6 @@ public class PlayerMovement : MonoBehaviour
     public float fastFallMultiplier = 1.5f; // Press down in-air = drop faster
     private float yVelocity;
 
-    /////////////////////////////////// TEST ///////////////////////////////////
-    [SerializeField] private AudioClip jumpClip;
-    ////////////////////////////////////////////////////////////////////////////
-
     [Header("Slide")]
     public float slideDuration = 0.6f;
     public float slideHeight = 1.0f;
