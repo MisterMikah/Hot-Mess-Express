683bbbe0aee704301c8a6485371c14a1dd10efda
IAAreval0 <143051271+IAAreval0@users.noreply.github.com>
2025-12-13 23:48:28 -0800

    Added TextMeshProEssentials, Main Menu, Settings Menu
    - also added Main Menu Scene, Settings Menu Scene, Scene_1 to build settings

---
+++ Assets/Menus/Main Menu/MainMenuScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public static Boolean prevSceneMain = false;
+    public void PlayGame() //starts game when press start button
+    {
+        SceneManager.LoadScene("Scene_1"); 
+        //REMINDERS:
+        //      - change string name from "Scene_1" to whatever the real scene will 
+        //          officially be called
+        //      - in build settings make sure to add the scenes that it will navigate to 
+        //             - how to get to build settings: file -> build settings
+    }
+
+    public void LoadSettingsMenu() //loads settings menu when press settings button
+    {
+        prevSceneMain = true;
+        SceneManager.LoadScene("Settings Menu");
+    }
+
+    public void QuitGame() //quits game when press exit button
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
