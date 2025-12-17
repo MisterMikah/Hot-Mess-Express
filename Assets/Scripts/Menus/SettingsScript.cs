5af6dafa95afd3c21f084f7a8a4bf6b1bb9ae4ef
IAAreval0 <143051271+IAAreval0@users.noreply.github.com>
2025-12-16 22:50:11 -0800

    started mixer and tried to put sound effects in slider

--- Assets/Scripts/Menus/SettingsScript.cs
+++ Assets/Scripts/Menus/SettingsScript.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
     [SerializeField] private AudioClip buttonPressClip;
+    [SerializeField] private AudioClip slidingPopClip;
+    [SerializeField] public Slider volumeSlider; 
     public void LoadPrevScene() //return to previous screen (Main Menu or Pause Screen) when press back button
     {
         //scuffed as hell, may change later should better solution present itself
@@ -22,4 +26,19 @@ public class SettingsScript : MonoBehaviour
 
     }
 
+      
+    //Doesn't sound good cause it gets called every time slider value changes
+    public void SliderSounds() //may delete
+    {
+        //make temp object that holds slider
+        Debug.Log(volumeSlider.value);
+
+        //test 
+        if ((Math.Round(volumeSlider.value) % 0.2) == 0) //doesn't work bc they aren't straight but divisible 0.02 ever (you get vals like 0.01231095)
+        {   //tried rounding, may have to scrap this idea
+
+            Debug.Log("In if statement");
+            SoundFXManager.instance.PlaySoundFXClip(slidingPopClip, Camera.main.transform, 1f);
+        }
+    }
 }
